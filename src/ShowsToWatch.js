8274662c6df6c0dba7417d494ab8e6e47df7601c
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-01-16 19:02:11 -0600

    fixed shows

--- src/ShowsToWatch.js
+++ src/ShowsToWatch.js
@@ -19,10 +19,15 @@ export default function ShowsToWatch() {
   const colRef = collection(db, "currentShows");
 
   const [currentSeason, setcurrentSeason] = useState("");
+  const [streaming_service, setStreaming_service] = useState("");
 
   const [dataBaseItems, setDatabaseItems] = useState([]);
   const [updating, setUpdating] = useState(false);
+  const [updatingSeason, setUpdatingSeason] = useState(false)
+  const [updatingStreamingService, setUpdatingStreamingService] =useState(false)
+  const [editedSeason,setEditedSeason] = useState("")
   const [editedEntry, setEditedEntry] = useState("");
+  const [editedStreamingService, setEditedStreamingService] = useState("")
 
   const [form, setForm]= useState({
     show: "",
@@ -39,7 +44,8 @@ export default function ShowsToWatch() {
       show: form.show,
       active: false,
       completed: false,
-      currentSeason: currentSeason
+      currentSeason: currentSeason,
+      streaming_service:streaming_service
     });
     setcurrentSeason("")
 
@@ -65,16 +71,48 @@ export default function ShowsToWatch() {
     fetchItems();
   }, []);
 
-  function updateItem(itemToUpdate) {
+  function updateCurrentSeason(itemToUpdate) {
     console.log("To be UPDATED " + itemToUpdate);
     if (itemToUpdate !== "") {
       console.log("Updating");
       const docToUpdate = doc(db, "currentShows", itemToUpdate);
       updateDoc(docToUpdate, {
+        currentSeason: editedSeason,
+      }).then(() => {
+        console.log("It supposed to be updated");
+      });
+      setUpdatingSeason(false);
+      setEditedSeason("")
+      fetchItems();
+    }
+  }
+  function updateStreamingService(itemToUpdate) {
+    console.log("To be UPDATED " + itemToUpdate);
+    if (itemToUpdate !== "") {
+      console.log("Updating");
+      const docToUpdate = doc(db, "currentShows", itemToUpdate);
+      updateDoc(docToUpdate, {
+        streaming_service: editedStreamingService,
+      }).then(() => {
+        console.log("It supposed to be updated");
+      });
+      setUpdatingStreamingService(false);
+      setEditedStreamingService("")
+      fetchItems();
+    }
+  }
+  function updateShow(itemToUpdate) {
+    console.log("To be UPDATED " + itemToUpdate);
+    if (itemToUpdate !== "") {
+      console.log("Updating");
+      const docToUpdate = doc(db, "currentShows", itemToUpdate);
+      if(editedEntry != "")
+      updateDoc(docToUpdate, {
         show: editedEntry,
       }).then(() => {
         console.log("It supposed to be updated");
       });
+      setEditedEntry("")
       setUpdating(false);
 
       fetchItems();
@@ -151,6 +189,13 @@ export default function ShowsToWatch() {
         type="text"
         name="currentSeason"/>
 
+        <label>Streaming Service</label>
+        <input 
+        value={streaming_service}
+        onChange={(e)=>setStreaming_service(e.target.value)}
+        type="text"
+        name="currentSeason"/>
+
         <button className="add-post" onClick={addItem}>Add show</button>
       </form>
 
@@ -160,10 +205,124 @@ export default function ShowsToWatch() {
       
         <div className=" item-grid">
           {dataBaseItems.map((database, id) => (
-            <div className={updating ? "insertedItem-tall" : "insertedItem-short"} key={database.id}>
-          <p> Show: {database.show}</p>    
-          {    database.currentSeason ? <p> Current Season: {database.currentSeason }</p>  : <></>            
-}
+            <div className={updating || updatingSeason || updatingStreamingService? "insertedItem-tall" : "insertedItem-short"} key={database.id}>
+          <p className="show">   {database.show}  <br/>   
+          <button
+                className="edit"
+                onClick={(e) => {
+                  setUpdating(true);
+                  setUpdatingSeason(false)
+                  setUpdatingStreamingService(false)
+                }}
+              >
+                Edit
+              </button> {updating ?  <button  
+              className="cancel"  
+              onClick={(e) => {
+                  setUpdating(false);
+                }}>Cancel</button> 
+                  : <></>}
+           {updating ? (
+                <div className="updating-show">
+                  {" "}
+                  <p>Change show to:</p>
+                  <input
+                    value={editedEntry}
+                    onChange={(e) => setEditedEntry(e.target.value)}
+                  />{" "}
+                  <button
+                    onClick={(e) => {
+                      updateShow(database.id);
+                    }}
+                  >
+                    Save
+                  </button>
+                 
+                </div>
+              ) : (
+                <div></div>
+              )}</p>  
+               <p> Current Season: {database.currentSeason }
+            <button
+                className="edit"
+                onClick={(e) => {
+                  setUpdatingSeason(true);
+                  setUpdating(false)
+                  setUpdatingStreamingService(false)
+                }}
+              >
+                Edit
+              </button>
+              {updatingSeason ?  <button  
+              className="cancel"  
+              onClick={(e) => {
+                  setUpdatingSeason(false)
+                  
+                }}>Cancel</button>  : <></>}
+             
+              {updatingSeason ? (
+                <div>
+                  {" "}
+                  <p>Change season to:</p>
+                  <input
+                    value={editedSeason}
+                    onChange={(e) => setEditedSeason(e.target.value)}
+                  />{" "}
+                  <button
+                    onClick={(e) => {
+                      updateCurrentSeason(database.id);
+                    }}
+                  >
+                    Save
+                  </button>
+                 
+                </div>
+              ) : (
+                <div></div>
+              )}    
+          
+            </p>    
+            <p> Streaming Service: {database.streaming_service }
+            <button
+                className="edit"
+                onClick={(e) => {
+                  setUpdatingStreamingService(true);
+                  setUpdating(false);
+                  setUpdatingSeason(false);
+                }}
+              >
+                Edit
+              </button>
+              {updatingStreamingService ?  <button  
+              className="cancel"  
+              onClick={(e) => {
+                  setUpdatingStreamingService(false)
+                }}>Cancel</button> : <></>}
+             
+              {updatingStreamingService ? (
+                <div>
+                  {" "}
+                  <p>Change streaming service to:</p>
+                  <input
+                    value={editedStreamingService}
+                    onChange={(e) => setEditedStreamingService(e.target.value)}
+                  />{" "}
+                  <button
+                    onClick={(e) => {
+                      updateStreamingService(database.id);
+                    }}
+                  >
+                    Save
+                  </button>
+                  
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+            </p>  
+          
+          
           
           <div className="active-buttons-container">
               <div 
@@ -189,34 +348,9 @@ export default function ShowsToWatch() {
                 )}
               </div>
               </div>
-              {updating ? (
-                <div>
-                  {" "}
-                  <p>Change show to:</p>
-                  <input
-                    value={editedEntry}
-                    onChange={(e) => setEditedEntry(e.target.value)}
-                  />{" "}
-                  <button
-                    onClick={(e) => {
-                      updateItem(database.id);
-                    }}
-                  >
-                    Save
-                  </button>
-                </div>
-              ) : (
-                <div></div>
-              )}
+    
               <br />
-              <button
-                className="edit"
-                onClick={(e) => {
-                  setUpdating(true);
-                }}
-              >
-                Edit
-              </button>
+           
               <button
                 className="delete"
                 onClick={(e) => {
